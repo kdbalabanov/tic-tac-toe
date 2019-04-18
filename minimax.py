1396e11e583022cb5582f99a12eec48ed39512e5
kdbalabanov <k.d.balabanov@gmail.com>
2019-04-18 23:52:03 +0100

    Started work on the MiniMax algorithm that will play against the player

---
+++ minimax.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+class MiniMax:
+    def __init__(self):
+        self._current_eval = 0
+        self._branches = []
+
+    def get_eval(self):
+        return 0
+
+    def solve(self):
+        return 0
